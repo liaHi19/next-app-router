7ff928b0d7158c01cf27c28c1b111f01d8db485a
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-01-05 12:12:29 +0000

    feat: adding mealDetail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,38 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "@/lib/meals";
+
+import styles from "./page.module.css";
+
 export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>{params.slug}</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
