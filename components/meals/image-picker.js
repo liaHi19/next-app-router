cb31157ffe0d4987e234f2742b088c3569fe0210
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-01-07 16:29:53 +0000

    feat: adding save meal functionality

---
+++ components/meals/image-picker.js
@@ -0,0 +1,65 @@
+"use client";
+import { useRef, useState } from "react";
+import styles from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+  const [pickedImage, setPickedImage] = useState();
+
+  const handlePickClick = () => {
+    imageInput.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {pickedImage ? (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          ) : (
+            <p>No image picked yet.</p>
+          )}
+        </div>
+        <input
+          className={styles.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          {" "}
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
