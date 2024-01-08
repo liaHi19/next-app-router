13ad7e9b5c404e071e6c85113fd063f4d7ac6621
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-01-08 08:20:09 +0000

    refactorL adding validation to shareMeal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,15 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
 import styles from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={styles.header}>
@@ -12,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -41,8 +48,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
